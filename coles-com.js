022eb12394c2fb8236a764f85e7ead481b74980c
Imranul Hasan <imranulhasan73@gmail.com>
2024-01-28 16:43:36 +0600

    webscrapping going on

--- coles-com.js
+++ coles-com.js
@@ -3,35 +3,59 @@ import cheerio from 'cheerio';
 import fs from 'fs';
 
 const url = 'https://www.coles.com.au/browse/dairy-eggs-fridge?pid=homepage_cat_explorer_dairy_eggs_fridge';
+const mainDomain="https://www.coles.com.au/";
 const visitedURLs = []; 
 const productURLs = new Set(); 
-const paginationQueueURLsToVisit = ["https://www.coles.com.au/browse/dairy-eggs-fridge?pid=homepage_cat_explorer_dairy_eggs_fridge"]; 
+ const paginationQueueURLsToVisit = ["/browse/dairy-eggs-fridge?pid=homepage_cat_explorer_dairy_eggs_fridge"];
+const maxPages = 50;
+const products=[];
+let count=0;
 
 async function webSpider() {     
-    //  const pageHTML = await axios.get(url);
-    //  const $ = cheerio.load(pageHTML.data); 
-    // // fs.writeFileSync('./page.txt', pageHTML.data);
 
-    ////////////////Get the Paginations//////////////
-    const paginationURLRoot = paginationQueueURLsToVisit.pop();
-    const pageHTML1 = await axios.get(paginationURLRoot);
-    visitedURLs.push(paginationURLRoot);
-		const $ = cheerio.load(pageHTML1.data); 
-		$('[data-testid="pagination"] li').each((index, element) => {
-      console.log($(element).find("a").attr("href"))
-		});
-    console.log(paginationQueueURLsToVisit);
-    ///////////////////////////
+    ////////////////Get the Paginations//////////////    
+    while(paginationQueueURLsToVisit.length !== 0 && visitedURLs.length <= maxPages  ){
+      const paginationURLRoot = paginationQueueURLsToVisit.pop();
+      if(!paginationURLRoot || paginationURLRoot==='undefined' || paginationURLRoot===undefined){
+        break;
+      }
+      const pageHTML1 = await axios.get(`${mainDomain}${paginationURLRoot}`);
+      visitedURLs.push(paginationURLRoot);
+      const $ = cheerio.load(pageHTML1.data);
 
-    // const productTiles = $('[data-testid="product-tile"]');
-    // productTiles.each((index, element) => {
-    //   // Process each element as needed
-    //   const productTitle = $(element).find('.product__title').text().trim();
-    //   const productPrice = $(element).find('.price__value').text();
-    //   const cleanedPrice = productPrice.replace(/\$([0-9]+\.[0-9]+)\$\1/, '$$$1');     
-    //   console.log(`Product Title ${index + 1}: ${productTitle} Price: ${cleanedPrice}`);
-  // });
+      ///////////// Get Products Information ////////////
+        const productTiles = $('[data-testid="product-tile"]');
+        let onPageProducts=[];
+        let paginationObj={
+          "paginationUrl":`${mainDomain}${paginationURLRoot}`
+        };
+        productTiles.each((index, element) => {
+          const productTitle = $(element).find('.product__title').text().trim();
+          const productPrice = $(element).find('.price__value').text();
+          const cleanedPrice = productPrice.replace(/\$([0-9]+\.[0-9]+)\$\1/, '$$$1');
+          console.log(`Product Title ${index + 1}: ${productTitle} Price: ${cleanedPrice}`);
+          let productInfo={productTitle, productPrice:cleanedPrice};
+          onPageProducts.push(JSON.stringify(productInfo));
+          count++;
+      });      
+      paginationObj['onPageProducts']=onPageProducts;
+      products.push(paginationObj);
 
+      ///////////Now Get the Paginations Number //////////
+      $('[data-testid="pagination"] li').each((index, element) => {
+        const paginationPageUrl = $(element).find("a").attr("href");
+        if(!visitedURLs.includes(paginationPageUrl) && !paginationQueueURLsToVisit.includes(paginationPageUrl)){
+          paginationQueueURLsToVisit.push(paginationPageUrl)
+        }
+      });
+    }
+    let obj={
+      "totalProducts":count,
+      products,
+    }
+      // console.log("Products:", obj);
+    const productsString = JSON.stringify(obj, null, 2);
+    fs.writeFileSync('./products.txt', productsString);
 } 
 
  webSpider();
